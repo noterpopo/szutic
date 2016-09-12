783cc700f0d3eee330ca1c0fab5ccee40103d084
noterpopo <popo54336@outlook.com>
2016-09-12 09:45:09 +0800

    Add files via upload

---
+++ demo2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+int main ()
+{
+	int num = 0;
+	cout<<"请输入一个正整数：";
+    if(cin>>num&&num>0)
+    	for(int i=1;i<=num;++i)
+    		cout<<i<<"I LOVE SZUTIC"<<std::endl;
+    else
+    	cout<<"I LOVE 深大";
+
+	return 0;
+}
